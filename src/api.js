4080d7164586c793376f6bb3bd5c1590b8587a05
SethTheMountain <smsorrell27@gmail.com>
2025-02-20 17:28:33 -0500

    added render url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'http://localhost:5003/api',
-});
+const api = axios.create({ baseURL: ${process.env.REACT_APP_API_URL}api});
 
 export default api;
